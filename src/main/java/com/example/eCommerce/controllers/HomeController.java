3134b0830821c0be822a65db3bfcda24269a90b8
Ahmet Çevrim <purplecevrim@gmail.com>
2025-02-12 04:05:19 +0300

    moved

--- src/main/java/com/example/eCommerce/controllers/HomeController.java
+++ src/main/java/com/example/eCommerce/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.eCommerce;
+package com.example.eCommerce.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
